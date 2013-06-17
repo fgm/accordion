8c18309f0bc142d62442c2a044ac42de3853a1e6
Frederic G. MARAND <fgm@osinet.fr>
2013-06-17 21:44:35 +0200

    Increase test coverage for initClasses().
    - new test runer to simplify runner HTML.

--- spec/101.test.js
+++ spec/101.test.js
@@ -1,5 +1,45 @@
 describe("Jasmine 101", function() {
   it("Expect accordion to be defined", function() {
-    expect(true).toBeDefined(true);
+    expect(accordion).toBeDefined(true);
+  });
+});
+
+
+describe('Accordion init', function () {
+  var $;
+  var backup;
+  var root = '.accordion';
+
+  // Preserve markup and jQuery.
+  beforeEach(function () {
+    $ = accordion.$;
+    backup = $(root).html();
+  });
+
+  // Restore initial markup.
+  afterEach(function () {
+    $(root).html(backup);
+  });
+
+  // This test checks initClasses() for headers.
+  it('Should contain exactly one active header.', function () {
+    expect($('h2.accordion-active').length).toBe(1);
+  });
+  it('Should have tagged all its headers and only them', function () {
+    expected = $(root + ' h2').length;
+    actual = $('.accordion-header').length;
+    expect(actual).toBe(expected);
+  });
+
+  // This test checks initClasses() for panes.
+  it('Should contain exactly three panes', function () {
+    expected = $(root + ' ul').length;
+    actual = $('.accordion-pane').length;
+    expect(actual).toBe(expected);
+  });
+
+  // This test checks initClasses() for items.
+  it('Should contain exactly one active item.', function () {
+    expect($('li.accordion-active').length).toBe(1);
   });
 });
